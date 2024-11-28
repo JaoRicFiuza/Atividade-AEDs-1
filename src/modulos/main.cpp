a6cab2fc3be2ccee9b3e9ccde2fed4002ee28776
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2024-11-28 18:57:26 -0300

    Funcionalidade de Cadastrar Passageiro

--- src/modulos/main.cpp
+++ src/modulos/main.cpp
@@ -1,155 +1,382 @@
 #include <iostream>
 #include <string.h>
-#include <fstream> //Biblioteca para manipulação de arquivos
-#include <string>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
-
+// CLASSES
 class Pessoa {
 protected:
     int codigo;
     string nome;
     string endereco;
     int tel;
-
-public:
-    Pessoa() : codigo(0), tel(0) {}
-
-    void SetDadosPessoa(int cod, string n, string end, int t) {
-        codigo = cod;
-        nome = n;
-        endereco = end;
-        tel = t;
+public: // GETTERS e SETTERS
+    int getCodigo() {
+        return codigo;
     }
 
-    string GetDadosPessoa() {
-        return "Codigo: " + to_string(codigo) + "\nNome: " + nome + "\nEndereco: " + endereco + "\nTelefone: " + to_string(tel);
+    void setCodigo(int cod) {
+        codigo = cod;
+    }
+
+    string getNome() {
+        return nome;
+    }
+
+    void setNome(string n) {
+        nome = n;
+    }
+
+    string getEndereco() {
+        return endereco;
+    }
+
+    void setEndereco(string e) {
+        endereco = e;
+    }
+
+    int getTel() {
+        return tel;
+    }
+
+    void setTel(int t) {
+        tel = t;
     }
 };
 
 class Passageiro : public Pessoa {
-private:
+protected:
     int codPassageiro;
     bool fidelidade;
-
-public:
-    Passageiro() : codPassageiro(0), fidelidade(false) {}
-
-    void CadastrarPassageiro (int cod, string n, string end, int t, int codPass, bool fid) {
-        SetDadosPessoa(cod, n, end, t);
-        codPassageiro = codPass;
-        fidelidade = fid;
+public: // GETTERS e SETTERS
+    int getCodPassageiro() {
+        return codPassageiro;
     }
 
-    string GetDadosPassageiro() {
-    return GetDadosPessoa() + "\nCodigo Passageiro: " + to_string(codPassageiro) + "\nFidelidade: " + (fidelidade ? "Sim" : "Nao") + "\n";
-}
+    void setCodPassageiro(int codPass) {
+        codPassageiro = codPass;
+    }
 
+    bool getFidelidade() {
+        return fidelidade;
+    }
+
+    void setFidelidade(bool f) {
+        fidelidade = f;
+    }
 };
 
-class Tripulacao : public Pessoa{
-    int cargo // Piloto= 1 Copiloto= 2 Comissario= 3
+class Tripulacao : public Pessoa {
+protected:
+    int cargo;
+public: // GETTERS e SETTERS
+    int getCargo() {
+        return cargo;
+    }
 
-    Pessoa.codigo;
-    Pessoa.endereco;
-    Pessoa.nome;
-    Pessoa.tel;
+    void setCargo(int c) {
+        cargo = c;
+    }
 };
 
-class Reserva : public Passageiro , public Voo, public Assento{
-    Passageiro.codPassageiro;
-    Passageiro.fidelidade;
-    Voo.codVoo;
-    Assento.numAssento;
-};
-
-class Assento : public Reserva, public Voo{
-    int numAssento;
-    bool status;
-    Voo.codVoo;
-};
-
-class Voo{
+class Voo {
+protected:
     int codAviao;
     int codComisario;
     int codPiloto;
     int codCopiloto;
     int codVoo;
-
     int data;
     int hora;
     string destino;
-    float tarifa;
-    
     bool status;
+    float tarifa;
+public: // GETTERS e SETTERS
+    int getCodAviao() {
+        return codAviao;
+    }
+
+    void setCodAviao(int cod) {
+        codAviao = cod;
+    }
+
+    int getCodComisario() {
+        return codComisario;
+    }
+
+    void setCodComisario(int cod) {
+        codComisario = cod;
+    }
+
+    int getCodPiloto() {
+        return codPiloto;
+    }
+
+    void setCodPiloto(int cod) {
+        codPiloto = cod;
+    }
+
+    int getCodCopiloto() {
+        return codCopiloto;
+    }
+
+    void setCodCopiloto(int cod) {
+        codCopiloto = cod;
+    }
+
+    int getCodVoo() {
+        return codVoo;
+    }
+
+    void setCodVoo(int cod) {
+        codVoo = cod;
+    }
+
+    int getData() {
+        return data;
+    }
+
+    void setData(int d) {
+        data = d;
+    }
+
+    int getHora() {
+        return hora;
+    }
+
+    void setHora(int h) {
+        hora = h;
+    }
+
+    string getDestino() {
+        return destino;
+    }
+
+    void setDestino(string dest) {
+        destino = dest;
+    }
+
+    bool getStatus() {
+        return status;
+    }
+
+    void setStatus(bool s) {
+        status = s;
+    }
+
+    float getTarifa() {
+        return tarifa;
+    }
+
+    void setTarifa(float t) {
+        tarifa = t;
+    }
 };
 
-//Funções
+class Assento {
+protected:
+    int numAssento;
+    bool status;
+public: // GETTERS e SETTERS
+    int getNumAssento() {
+        return numAssento;
+    }
 
-void salvarPassageiroNoArquivo(Passageiro passageiro) {
+    void setNumAssento(int num) {
+        numAssento = num;
+    }
+
+    bool getStatus() {
+        return status;
+    }
+
+    void setStatus(bool s) {
+        status = s;
+    }
+};
+
+class Reserva : public Passageiro, public Voo, public Assento {
+protected:
+public:
+};
+
+//FUNÇÕES DO CODIGO
+
+// Função para criar códigos sequenciais de passageiros
+int gerarCodigo() {
+    ifstream arquivoEntrada("codigos_passageiros.txt");
+    ofstream arquivoSaida;
+    int ultimoCodigo = 0;
+
+    // Lê o último código do arquivo, se existir
+    if (arquivoEntrada.is_open()) {
+        arquivoEntrada >> ultimoCodigo;
+        arquivoEntrada.close();
+    }
+
+    // Incrementa o código
+    int novoCodigo = ultimoCodigo + 1;
+
+    // Salva o novo código no arquivo
+    arquivoSaida.open("codigos_passageiros.txt", ios::trunc); // Sobrescreve o arquivo
+    if (arquivoSaida.is_open()) {
+        arquivoSaida << novoCodigo;
+        arquivoSaida.close();
+    }
+
+    return novoCodigo;
+}
+
+// Função para salvar as informações do passageiro no arquivo
+void salvarPassageiroNoArquivo(Passageiro& p) {
     ofstream arquivo("passageiros.txt", ios::app); // Modo append
     if (arquivo.is_open()) {
-        arquivo << passageiro.GetDadosPassageiro() << "------------------\n";
+        arquivo << "Codigo do Passageiro: " << p.getCodPassageiro() << endl;
+        arquivo << "Nome: " << p.getNome() << endl;
+        arquivo << "Endereco: " << p.getEndereco() << endl;
+        arquivo << "Telefone: " << p.getTel() << endl;
+        arquivo << "Fidelidade: " << (p.getFidelidade() ? "Sim" : "Nao") << endl;
+        arquivo << "-------------------------" << endl;
         arquivo.close();
         cout << "Passageiro salvo com sucesso!" << endl;
     } else {
-        cout << "Erro ao salvar passageiro!" << endl;
+        cout << "Erro ao abrir o arquivo para salvar!" << endl;
     }
 }
 
-string CriarCodigoPassageiro() {
-    string linha;
-    int ultimoCodigo = 0;
-    ifstream arquivo("passageiros.txt");
+void cadastroPassageiro() {
+    Passageiro p; // Criando um objeto Passageiro
 
-    if (arquivo.is_open()) {
-        while (getline(arquivo, linha)) {
-            // Verifica se a linha contém o prefixo "Código do Passageiro: P"
-            if (linha.find("Código do Passageiro: P") == 0) {
-                int novoCodigo = stoi(linha.substr(23)); // Extrai o número do código
-                if (novoCodigo > ultimoCodigo) {
-                    ultimoCodigo = novoCodigo;
-                }
-            }
-        }
-        arquivo.close();
-    } else {
-        cout << "Erro ao abrir o arquivo!" << endl;
-    }
-
-    ultimoCodigo++;  // Incrementa o último código para o próximo
-    return "P" + to_string(ultimoCodigo); // Gera o novo código de passageiro com prefixo 'P'
-}
-
-void cadastroPassageiro(){
-    Passageiro p; // Cria um objeto Passageiro
-
-    string nome, endereco, codPassageiro;
+    string nome, endereco;
     int telefone;
-    char fidelidade;
+    char fidelidadeChar; // Para capturar entrada do usuário
+    bool fidelidade;     // Valor booleano derivado da entrada
 
-    // Criar funcao para gerar Cod/ Return Codigo do passageiro
-    cout << "Digite o nome do passageiro:";
-    cin >> nome;
-    cout << "Digiteo o endereco do passageiro:";
-    cin >> endereco;
-    cout << "Digite o telefone do passageiro:";
+    // Entrada de dados
+    cout << "Digite o nome do passageiro: ";
+    cin.ignore(); // Ignorar o buffer
+    getline(cin, nome);
+
+    cout << "Digite o endereco do passageiro: ";
+    getline(cin, endereco);
+
+    cout << "Digite o telefone do passageiro: ";
     cin >> telefone;
-    cout << "O passageiro gostaria de participar da fidelidade de Voo: (s/n)";
-    cin >> fidelidade;
 
-    codPassageiro = CriarCodigoPassageiro(); // Gera o código do passageiro
+    cout << "O passageiro deseja participar do programa de fidelidade? (s/n): ";
+    cin >> fidelidadeChar;
 
-    // Setando os dados no objeto Passageiro
-    p.CadastrarPassageiro(stoi(codPassageiro.substr(1)), nome, endereco, telefone, fidelidade == 's');
+    // Converte 's' ou 'n' em valor booleano
+    fidelidade = (fidelidadeChar == 's' || fidelidadeChar == 'S');
 
-    // Salvando o passageiro no arquivo
-    salvarPassageiroNoArquivo(p);
+    // Geração do código único para o passageiro
+    int CodPassageiro;
+
+    // Configurando os atributos do passageiro
+    p.setCodPassageiro(CodPassageiro);
+    p.setNome(nome);
+    p.setEndereco(endereco);
+    p.setTel(telefone);
+    p.setFidelidade(fidelidade);
+
+    // Exibindo os dados cadastrados
+    cout << "\nPassageiro cadastrado com sucesso!\n";
+    cout << "Codigo do Passageiro: " << p.getCodPassageiro() << endl;
+    cout << "Nome: " << p.getNome() << endl;
+    cout << "Endereco: " << p.getEndereco() << endl;
+    cout << "Telefone: " << p.getTel() << endl;
+    cout << "Fidelidade: " << (p.getFidelidade() ? "Sim" : "Nao") << endl;
 }
 
-int main()
-{
-    cadastroPassageiro();
+void menu() {
+    cout << "        __|__" << endl;
+    cout << "--@--@--(_)--@--@--" << endl;
+    cout << "\n==== MENU ====" << endl;
+    cout << "1. Cadastrar Passageiro" << endl;
+    cout << "2. Cadastrar Tripulacao" << endl;
+    cout << "3. Cadastrar Voo" << endl;
+    cout << "4. Cadastrar Assento" << endl;
+    cout << "5. Realizar Reserva" << endl;
+    cout << "6. Baixa em Reservas" << endl;
+    cout << "7. Pesquisa" << endl;
+    cout << "8. Consultar Programa de Fidelidade" << endl;
+    cout << "9. Sair" << endl;
+    cout << "=====================" << endl;
+}
+
+void cadastroTripulacao(){
+  cout <<"ENTROU";
+}
+
+void cadastroVoo(){
+    cout <<"ENTROU";
+}
+
+void cadastroAssento(){
+    cout <<"ENTROU";
+}
+
+void reserva(){
+    cout <<"ENTROU";
+
+}
+
+void baixaReserva(){
+    cout <<"ENTROU";
+}
+
+void pesquisa(){
+    cout <<"ENTROU";
+
+}
+
+void programaFid(){
+    cout <<"ENTROU";
+}
+
+int main() {
+   int op;
+    while (op != 9)
+    {
+        menu();
+        cout << "Escolha uma opcao (1-9): ";
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                cadastroPassageiro();
+                break;
+            case 2:
+                cadastroTripulacao();
+                break;
+            case 3:
+                cadastroVoo();
+                break;
+            case 4:
+                cadastroAssento();
+                break;
+            case 5:
+                reserva();
+                break;
+            case 6:
+                baixaReserva();
+                break;
+            case 7:
+                pesquisa();
+                break;
+            case 8:
+                programaFid();
+                break;
+            case 9:
+                cout << "Saindo do sistema" << endl;
+                return 0;
+            default:
+                cout << "Opção inválida! Por favor, escolha um número entre 1 e 9." << endl;
+        }
+    }
+
     return 0;
 }
-
